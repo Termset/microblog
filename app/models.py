4829e083c75682360a15bcc3b7372b38ea047fcb
Termset <18351926130@163.com>
2018-09-27 22:46:26 +0800

    关注与被关注

--- app/models.py
+++ app/models.py
@@ -60,7 +60,9 @@ class User(db.Model):
         return self.followed.filter(followers.c.follower_id == user.id).count() > 0
 
     def followed_posts(self):
-        return Post.query.join(followers, (followers.c.follower_id == Post.user_id)).filter(followers.c.follower_id == self.id).order_by(Post.timestamp.desc())
+        return Post.query.join(followers, (followers.c.follower_id == Post.user_id)).filter(
+            followers.c.follower_id == self.id).order_by(Post.timestamp.desc())
+
     @staticmethod
     def make_unique_nickname(nickname):
         if User.query.filter_by(nickname=nickname).first() is None:
